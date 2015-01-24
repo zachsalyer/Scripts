beaea26df63947a15037888b7d9da795ed5cc9d0
Aaron Bonnell-Kangas <abk@aaronbeekay.info>
2015-01-24 00:28:33 -0500

    Rewrite sync_logfiles() to use cross-correlation instead of minimizing difference

--- sync_logfiles/sync_logfiles.m
+++ sync_logfiles/sync_logfiles.m
@@ -1,25 +1,21 @@
-function [offset, fit] = sync_logfiles(ts1, ts2)
-	% Set bounds on the offset s.t. the two dataseries must overlap
-	% by at least 10% of the length of the shorter one.
-	shortest = min( range( ts1.Time ), range( ts2.Time ));
-	bound1 = ts2.Time(1) - ts1.Time(end);
-	bound2 = ts2.Time(end) - ts1.Time(1);
-	
-	% TODO: add 10% overlap requirement
-	offset_min = min( bound1, bound2 ) + 0.1*shortest;
-	offset_max = max( bound1, bound2 ) - 0.1*shortest;
-	
-	[offset, fit] = fminbnd( @(offset) offset_error(ts1, ts2, offset), offset_min, offset_max );
-end
+function offset = sync_logfiles(ts1, ts2)
+	% This function returns the offset that should be APPLIED to ts2 in
+	% order to synchronize ts1 and ts2. 
+	% 
+	% SO: ts2.Time + offset(ts1, ts2) will match ts1.
 
-function error = offset_error(ts1, ts2, offset)
-	% Apply offset
-	ts2.Time = ts2.Time + offset;
+	% Resample both timeseries to a suitably small timestep.
+	% Our WS200 log interval is 200 ms, so we will use 10 ms here.
+	precision = 0.01;	% second
+	ts1 = resample( ts1, 0:precision:range(ts1.Time) );
+	ts2 = resample( ts2, 0:precision:range(ts2.Time) );
 	
-	% Synchronize on union
-	[ts1 ts2] = synchronize(ts1, ts2, 'Union');
-	avg = mean( ts2.Data - ts1.Data );
-	error = sum( (ts2.Data-ts1.Data).^2 );		% SSE
-	%error = rms( ts2.Data-ts1.Data );			% RMS
-	%error = sum( (ts2.Data-ts1.Data).^2 )/sum( (ts2.Data-ts1.Data)-avg ); % R^2
+	% Offset them so they start at the same time. This is part of our total
+	% offset
+	offset = ts1.Time(1) - ts2.Time(1);
+	
+	% Find the cross-correlation between the two timeseries.
+	[xc, lags] = xcorr(ts1.Data, ts2.Data);
+	[m,i] = max(xc);
+	offset = offset + lags(i)*precision;
 end
