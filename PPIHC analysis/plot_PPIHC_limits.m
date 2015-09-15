0012f087abae537403e161a4ec9dd33d66d4320e
Aaron Bonnell-Kangas <abk@aaronbeekay.info>
2015-09-15 00:32:33 -0400

    Take advantage of new data structure

--- PPIHC analysis/plot_PPIHC_limits.m
+++ PPIHC analysis/plot_PPIHC_limits.m
@@ -3,67 +3,31 @@
 
 % If the 'Race.mat' file is not already on the MATLAB path, make sure it is
 % added here.
-
+addpath('/Users/abk/Dropbox/Documents/OSU/Buckeye Current/Testing/2015-06-xx Pikes Peak race');
 
 % load race data
 clear; clc; close all
-load 'Race.mat';
-
-%% Race parameters.
-
-% Race start and end time offsets, in seconds.
-race_start_time		= 166.9;
-race_end_time		= 847.1;	% note: this is not perfectly precise
-
-
-% Re-index the timeseries so that 0 = race start
-% Later, this should be done automatically
-MotorVelocity.Time			= MotorVelocity.Time - race_start_time;
-VehicleVelocity.Time		= VehicleVelocity.Time - race_start_time;
-LimitFlags.Time				= LimitFlags.Time - race_start_time;
-MaxCellTemp.Time			= MaxCellTemp.Time - race_start_time;
-MotorCurrentSetpoint.Time	= MotorCurrentSetpoint.Time - race_start_time;
-PhaseCurrentB.Time			= PhaseCurrentB.Time - race_start_time;
-BusVoltage.Time				= BusVoltage.Time - race_start_time;
-Odometer.Time				= Odometer.Time - race_start_time;
+%load 'Race.mat';
+load 'PPIHC_15';			% re-structured race data
 
 %% Construct limit timeseries
-
-% Resample limit vectors to common time vector
-% TODO: This should be done in an outside function
-CommonTime.step		= 0.1;		% seconds
-CommonTime.min		= max( min(LimitFlags.Time), min(BatteryLimit.Time) );
-
-%CommonTime.max		= min( max(LimitFlags.Time), max(BatteryLimit.Time) );
-CommonTime.max		= race_end_time+2;		% TEMPORARY HACK
-
-CommonTime.vector	= CommonTime.min:CommonTime.step:CommonTime.max;
-
-LimitFlagsOrig		= LimitFlags;
-LimitFlags			= resample( LimitFlags, CommonTime.vector, 'zoh' );
-BatteryLimit		= resample( BatteryLimit, CommonTime.vector, 'zoh' );
-BusVoltage			= resample( BusVoltage, CommonTime.vector, 'zoh' );
-MotorCurrentSetpoint = resample( MotorCurrentSetpoint, CommonTime.vector, 'zoh' );
-PhaseCurrentB		= resample( PhaseCurrentB, CommonTime.vector, 'zoh' );
-
-
-CurrentLimit.Temperature			= timeseries((LimitFlags.Data == 2^6), CommonTime.vector);
-CurrentLimit.BusVoltageLower		= timeseries((LimitFlags.Data == 2^5), CommonTime.vector);
-CurrentLimit.BusVoltageUpper		= timeseries((LimitFlags.Data == 2^4), CommonTime.vector);
-CurrentLimit.BusCurrent				= timeseries((LimitFlags.Data == 2^3), CommonTime.vector);
-CurrentLimit.Velocity				= timeseries((LimitFlags.Data == 2^2), CommonTime.vector);
-CurrentLimit.MotorCurrent			= timeseries((LimitFlags.Data == 2^1), CommonTime.vector);
-CurrentLimit.OutputPWM				= timeseries((LimitFlags.Data == 2^0), CommonTime.vector);
+CurrentLimit.Temperature			= timeseries((Powertrain.Controller.LimitFlags.Data == 2^6), Powertrain.Controller.LimitFlags.Time);
+CurrentLimit.BusVoltageLower		= timeseries((Powertrain.Controller.LimitFlags.Data == 2^5), Powertrain.Controller.LimitFlags.Time);
+CurrentLimit.BusVoltageUpper		= timeseries((Powertrain.Controller.LimitFlags.Data == 2^4), Powertrain.Controller.LimitFlags.Time);
+CurrentLimit.BusCurrent				= timeseries((Powertrain.Controller.LimitFlags.Data == 2^3), Powertrain.Controller.LimitFlags.Time);
+CurrentLimit.Velocity				= timeseries((Powertrain.Controller.LimitFlags.Data == 2^2), Powertrain.Controller.LimitFlags.Time);
+CurrentLimit.MotorCurrent			= timeseries((Powertrain.Controller.LimitFlags.Data == 2^1), Powertrain.Controller.LimitFlags.Time);
+CurrentLimit.OutputPWM				= timeseries((Powertrain.Controller.LimitFlags.Data == 2^0), Powertrain.Controller.LimitFlags.Time);
 
 % The battery temperature limit only has an effect when the Tritium
 % "MotorCurrent" limit is active.
-CurrentLimit.BatteryTemperature			= BatteryLimit;
+CurrentLimit.BatteryTemperature			= Powertrain.Controller.BatteryLimit;
 CurrentLimit.BatteryTemperature.Data	= CurrentLimit.BatteryTemperature.Data & CurrentLimit.MotorCurrent.Data;
 
 % Similarly, the "ThrottlePosition" limit (i.e., motor current limited only
 % by throttle position) is only effective when the battery temperature limit
 % is not in effect.
-CurrentLimit.ThrottlePosition		= timeseries((LimitFlags.Data == 2^1), CommonTime.vector);		% MotorCurrent
+CurrentLimit.ThrottlePosition		= timeseries((Powertrain.Controller.LimitFlags.Data == 2^1), Powertrain.Controller.LimitFlags.Time);		% MotorCurrent
 CurrentLimit.ThrottlePosition.Data	= CurrentLimit.ThrottlePosition.Data & ~CurrentLimit.BatteryTemperature.Data;
 
 % Use a moving average to provide a better high-level view of the limit
@@ -81,33 +45,6 @@ CurrentLimitAvg.ThrottlePosition.Data	= smooth(double(CurrentLimit.ThrottlePosit
 CurrentLimitAvg.OutputPWM.Data			= smooth(double(CurrentLimit.OutputPWM.Data), span);
 CurrentLimitAvg.BatteryTemperature.Data	= smooth(double(CurrentLimit.BatteryTemperature.Data), span);
 
-%% Compare original limit data to resampled
-CurrentLimitOrig.Temperature			= timeseries((LimitFlagsOrig.Data == 2^6), LimitFlagsOrig.Time);
-CurrentLimitOrig.BusVoltageLower		= timeseries((LimitFlagsOrig.Data == 2^5), LimitFlagsOrig.Time);
-CurrentLimitOrig.BusVoltageUpper		= timeseries((LimitFlagsOrig.Data == 2^4), LimitFlagsOrig.Time);
-CurrentLimitOrig.BusCurrent				= timeseries((LimitFlagsOrig.Data == 2^3), LimitFlagsOrig.Time);
-CurrentLimitOrig.Velocity				= timeseries((LimitFlagsOrig.Data == 2^2), LimitFlagsOrig.Time);
-CurrentLimitOrig.MotorCurrent			= timeseries((LimitFlagsOrig.Data == 2^1), LimitFlagsOrig.Time);
-CurrentLimitOrig.OutputPWM				= timeseries((LimitFlagsOrig.Data == 2^0), LimitFlagsOrig.Time);
-
-CurrentLimitOrig.OutputPWM		= CurrentLimitOrig.OutputPWM.setinterpmethod('zoh');
-CurrentLimit.OutputPWM			= CurrentLimit.OutputPWM.setinterpmethod('zoh');
-
-figure(5); clf; hold on;
-ax1 = subplot(311);
-plot( CurrentLimitOrig.OutputPWM, '-*' );
-plot( CurrentLimit.OutputPWM, '-*' );
-
-ax2 = subplot(312); hold on;
-plot( BusVoltage );
-
-ax3 = subplot(313); hold on;
-plot( MotorCurrentSetpoint.*300, 'r' );
-plot( PhaseCurrentB, 'b' );
-
-linkaxes([ax1 ax2 ax3], 'x');
-%xlim([race_start_time race_end_time]);
-xlim([190 210]);
 
 %% test plots
 race_start_index	= find( CommonTime.vector > 0, 1);
